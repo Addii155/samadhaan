92f9529d3c7f17e35839d28252ce794634d6c5cf
Aditya Gupta <148351333+AaDiTYAKumaR1@users.noreply.github.com>
2025-04-19 11:09:28 +0530

    update MongoDB connection string and improve error logging; add multer dependency

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://aditysgoyal11:oQV3vGBNp4YE2Gxg@cluster0.wruvd5f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    await mongoose.connect("mongodb+srv://aditysgoyal11:hOmNIWD3H0jiXttm@cluster0.77vklin.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
     console.log(" MONGO DB CONNECTED ");
   } catch (err) {
-    console.error(err.message);
+    console.error("❌ MongoDB connection error:", err);
+
     process.exit(1);
   }
 };
