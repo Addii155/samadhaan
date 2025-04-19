c4f3bc7a653908a7d8c8ee4580127027dea33fd2
Aditya Gupta <148351333+AaDiTYAKumaR1@users.noreply.github.com>
2025-04-19 10:12:21 +0530

    dine

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/SAMADHAAN-YASH");
+    await mongoose.connect("mongodb+srv://aditysgoyal11:oQV3vGBNp4YE2Gxg@cluster0.wruvd5f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
     console.log(" MONGO DB CONNECTED ");
   } catch (err) {
